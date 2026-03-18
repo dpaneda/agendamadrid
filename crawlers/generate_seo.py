96857c52e902ff09e2e22a7c8ae4ca590ce7f022
Daniel Pañeda <dpaneda@gmail.com>
2026-03-18 19:15:45 +0100

    Fix sitemap: restore info.html entry

--- crawlers/generate_seo.py
+++ crawlers/generate_seo.py
@@ -194,6 +194,7 @@ def _generate_sitemap(today, day_dates):
     lines = ['<?xml version="1.0" encoding="UTF-8"?>',
              '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">']
     lines.append(f'  <url><loc>{BASE_URL}/</loc><changefreq>daily</changefreq><priority>1.0</priority></url>')
+    lines.append(f'  <url><loc>{BASE_URL}/info.html</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>')
     for ds in sorted(day_dates):
         lines.append(f'  <url><loc>{BASE_URL}/{ds}/</loc><changefreq>daily</changefreq><priority>0.8</priority></url>')
     lines.append('</urlset>')
