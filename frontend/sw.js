5f377915e993a50a6b6c95f7dc8b91998a28fb0e
Daniel Pañeda <dpaneda@gmail.com>
2026-03-24 14:42:06 +0100

    Disable service worker: self-unregister and clear all caches

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,32 +1,9 @@
-const CACHE_NAME = "agendamadrid-v2";
-
-// Activate: clean old caches
+// Unregister and clean up - SW disabled
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
   );
   self.clients.claim();
-});
-
-self.addEventListener("install", () => self.skipWaiting());
-
-// Network-first for everything: fast when online, works offline from cache
-self.addEventListener("fetch", (e) => {
-  // Skip non-GET requests
-  if (e.request.method !== "GET") return;
-
-  e.respondWith(
-    fetch(e.request)
-      .then((res) => {
-        // Cache successful responses
-        if (res.ok) {
-          const clone = res.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  self.registration.unregister();
 });
